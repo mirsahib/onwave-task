8397b33f90aa29e5b39319f1458917777226c159
Mir Sahib <mirsahib24@gmail.com>
2023-12-17 12:18:37 +0600

    :wrench: Add server port in env

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,10 @@
 const express = require('express');
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.SERVER_PORT||3001;
 
 app.get('/', (req, res) => {
+  console.log('process',process.env.SERVER_PORT);
   res.send('Hello, Node.js Server!');
 });
 
